d2d4c9705dfb9637da7d18345a84af07d183f3c5
AnhMinhTrinh <119995188+AnhMinhTrinh@users.noreply.github.com>
2025-01-02 11:26:18 +0700

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WEB_DEMO.Models;
+
+namespace WEB_DEMO.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<WEB_DEMO.Models.Food> Food { get; set; } = default!;
+    }
+
+    
+}

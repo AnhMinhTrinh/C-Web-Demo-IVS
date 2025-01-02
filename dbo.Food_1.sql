27c4a6aebcaa67bfbfa1b987818c6086bcca221b
AnhMinhTrinh <119995188+AnhMinhTrinh@users.noreply.github.com>
2025-01-02 11:35:31 +0700

    Add files via upload

---
+++ dbo.Food_1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Food] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (MAX) NULL,
+    [Description] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Food] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+

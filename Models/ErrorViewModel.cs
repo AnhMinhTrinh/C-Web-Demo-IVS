79eba23e254dc53c847796abb8214e999dccc3f7
AnhMinhTrinh <119995188+AnhMinhTrinh@users.noreply.github.com>
2025-01-02 11:20:43 +0700

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_DEMO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Food.cs
@@ -0,0 +1,16 @@
+﻿namespace WEB_DEMO.Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Food()
+        {
+
+        }
+    }
+}

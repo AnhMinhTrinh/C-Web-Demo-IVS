823759551db6b802555c2164ffe41a66d623e6af
AnhMinhTrinh <119995188+AnhMinhTrinh@users.noreply.github.com>
2025-01-02 11:22:56 +0700

    Add files via upload

---
+++ Controllers/FoodsController.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WEB_DEMO.Data;
+using WEB_DEMO.Models;
+
+namespace WEB_DEMO.Controllers
+{
+    public class FoodsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FoodsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Foods
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Food.ToListAsync());
+        }
+
+
+        // GET: Foods/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View();
+        }
+
+        // Post: Foods/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
+        {
+            var results = await _context.Food.Where(j => j.Name.Contains(SearchPhrase)).ToListAsync();
+
+            if ( results.Count == 0)
+            {
+                ViewData["SearchMessage"] = "No search results found";
+
+            }
+            else{
+                ViewData["SearchMessage1"] = "Your search has been found";
+            }
+            
+            return View("Index", results);
+
+
+
+
+        }
+
+
+
+
+
+        // GET: Foods/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var food = await _context.Food
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            return View(food);
+        }
+
+        // GET: Foods/Create
+
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: Foods/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description")] Food food)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(food);
+                await _context.SaveChangesAsync();
+                TempData["SuccessMessage"] = "Your recipe has been created successfully!";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(food);
+        }
+
+        // GET: Foods/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var food = await _context.Food.FindAsync(id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+            return View(food);
+        }
+
+        // POST: Foods/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Food food)
+        {
+            if (id != food.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(food);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FoodExists(food.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                TempData["SuccessEdit"] = "Your recipe has been edited successfully!";
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(food);
+        }
+
+        // GET: Foods/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var food = await _context.Food
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            return View(food);
+        }
+
+        // POST: Foods/Delete/5
+
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var food = await _context.Food.FindAsync(id);
+            if (food != null)
+            {
+                _context.Food.Remove(food);
+            }
+
+            await _context.SaveChangesAsync();
+            TempData["SuccessDelete"] = "Your recipe has been deleted successfully!";
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FoodExists(int id)
+        {
+            return _context.Food.Any(e => e.Id == id);
+        }
+    }
+}
